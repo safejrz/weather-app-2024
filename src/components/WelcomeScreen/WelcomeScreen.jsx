466e79df1d1c7800c43583ab3d8cba68b26ac7bf
Javier Rivera <ingjrz@gmail.com>
2024-03-26 19:56:53 -0600

    Add welcomescreen component

---
+++ src/components/WelcomeScreen/WelcomeScreen.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { PropTypes } from '@material-ui/core'
+
+const WelcomeScreen = ({ children }) => {
+    return (
+    <>
+    WelcomeScreen
+    </>
+  )
+}
+
+WelcomeScreen.propTypes = {
+    children : PropTypes.node,
+}
+
+export default WelcomeScreen
