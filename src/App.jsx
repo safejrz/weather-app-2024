94374dadff3d21786ded417513c4d80100efcfed
Javier Rivera <ingjrz@gmail.com>
2024-03-26 19:47:29 -0600

    Added app frame to the main page

--- src/App.jsx
+++ src/App.jsx
@@ -9,33 +9,23 @@ import CityPage from './pages/CityPage'
 import NotFoundPage from './pages/NotFoundPage'
 
 const App = () => {
-    return (
-        <Grid container
-            justify="center"
-            direction="row">
-            <Grid item
-                xs={12} 
-                sm={11}
-                md={10}
-                lg={8}>
-                <Router>
-                    <Switch>
-                        <Route exact path="/">
-                            <WelcomePage />
-                        </Route>
-                        <Route path="/main">
-                            <MainPage />
-                        </Route>      
-                        <Route path="/city">
-                            <CityPage />
-                        </Route> 
-                        <Route>
-                            <NotFoundPage />
-                        </Route>                                                           
-                    </Switch>
-                </Router>
-            </Grid>
-        </Grid>
+    return (        
+        <Router>
+            <Switch>
+                <Route exact path="/">
+                    <WelcomePage />
+                </Route>
+                <Route path="/main">
+                    <MainPage />
+                </Route>      
+                <Route path="/city">
+                    <CityPage />
+                </Route> 
+                <Route>
+                    <NotFoundPage />
+                </Route>                                                           
+            </Switch>
+        </Router>
     )
 }
 
