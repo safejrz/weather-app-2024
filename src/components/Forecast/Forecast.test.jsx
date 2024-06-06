a64c5e0697569ecb6d345cd536abe8400b82da4e
Javier Rivera <ingjrz@gmail.com>
2024-06-06 17:06:46 -0600

    unit test fixes

--- src/components/Forecast/Forecast.test.jsx
+++ src/components/Forecast/Forecast.test.jsx
@@ -7,14 +7,14 @@ const forecastItemList = [
 	{ hour: 6, state:"clouds", temperature:18, weekDay:"Viernes" },
 	{ hour: 12, state:"drizzle", temperature:18, weekDay:"Viernes" },
 	{ hour: 18, state:"clouds", temperature:19, weekDay:"Viernes" },
-	{ hour: 14, state:"rain", temperature:17, weekDay:"Sábado" },
-	{ hour: 14, state:"rain", temperature:17, weekDay:"Sábado" }, 
+	{ hour: 6, state:"rain", temperature:17, weekDay:"Sábado" },
+	{ hour: 12, state:"rain", temperature:17, weekDay:"Sábado" }, 
 ]
 
 test('Forecast render', async () => {
     // ¿Cómo encontrar los items?
     // findAllByTestId nos va a permitir encontrar cada item con esa marca
-
+ 
     const { findAllByTestId } = render(
             <Forecast 
                 forecastItemList={forecastItemList} />)
