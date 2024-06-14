31c64170cbd40d28de31396b1532401eff5eb970
Javier Rivera <ingjrz@gmail.com>
2024-06-14 17:22:26 -0600

    Context providers and hook useContext

--- src/App.jsx
+++ src/App.jsx
@@ -1,63 +1,38 @@
-import React, { useReducer } from 'react' //useState, useCallback, useMemo,
+import React from 'react' //useState, useCallback, useMemo,
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import WelcomePage from './pages/WelcomePage'
 import MainPage from './pages/MainPage'
 import CityPage from './pages/CityPage'
 import NotFoundPage from './pages/NotFoundPage'
 import ErrorBoundary from './generic/ErrorBoundary/ErrorBoundary'
-//import { texture } from 'three/examples/jsm/nodes/Nodes.js'
+import { WeatherContext } from './WeatherContext'
 
 const App = () => {
-    const initialValue = {
-        allWeather: {},
-        allChartData: {},
-        allForecastItemList: {}
-    }
-
-    const reducer = (state, action) => {
-        switch (action.type) {
-            case 'SET_ALL_WEATHER':
-                const weatherCity = action.payload
-                const newAllWeather = { ...state.allWeather, ...weatherCity }
-                return { ...state, allWeather: newAllWeather }
-            case 'SET_CHART_DATA':
-                const chartDataCity = action.payload 
-                const newAllChartData = { ...state.allChartData, ...chartDataCity }
-                return { ...state, allChartData: newAllChartData }
-            case 'SET_FORECAST_ITEM_LIST':
-                const forecastItemListCity = action.payload
-                const newAllForecastItemListCity = { ...state.allForecastItemList, ...forecastItemListCity }
-                return { ...state, allForecastItemList: newAllForecastItemListCity }
-            default:
-                return state 
-        }
-    }
-
-    const [state, dispatch] = useReducer(reducer, initialValue)
-
     return (
-        <Router>
-            <Switch>
-                <Route exact path="/">
-                    <WelcomePage />
-                </Route>
-                <Route path="/welcome">
-                    <WelcomePage />
-                </Route>
-                <Route path="/main">
-                    <MainPage data={state} actions={dispatch} />
-                </Route>
-                <Route path="/city/:countryCode/:city">
-                    <CityPage data={state} actions={dispatch} />
-                </Route>
-                <Route exact path="/error">
-                    <ErrorBoundary saludo="HOLA!!!" />
-                </Route>
-                <Route>
-                    <NotFoundPage />
-                </Route>
-            </Switch>
-        </Router>
+        <WeatherContext>
+            <Router>
+                <Switch>
+                    <Route exact path="/">
+                        <WelcomePage />
+                    </Route>
+                    <Route path="/welcome">
+                        <WelcomePage />
+                    </Route>
+                    <Route path="/main">
+                        <MainPage />
+                    </Route>
+                    <Route path="/city/:countryCode/:city">
+                        <CityPage />
+                    </Route>
+                    <Route exact path="/error">
+                        <ErrorBoundary saludo="HOLA!!!" />
+                    </Route>
+                    <Route>
+                        <NotFoundPage />
+                    </Route>
+                </Switch>
+            </Router>
+        </WeatherContext>
     )
 }
 
--- src/pages/CityPage.jsx
+++ src/pages/CityPage.jsx
@@ -1,4 +1,4 @@
-import React, { useMemo } from 'react'
+import React, { useContext, useMemo } from 'react'
 import Grid from '@material-ui/core/Grid'
 import LinearProgress from '@material-ui/core/LinearProgress'
 import AppFrame from './../components/AppFrame'
@@ -13,8 +13,11 @@ import { getCityCode } from './../utils/utils'
 import { getCountryNameByCountryCode } from './../utils/serviceCities'
 import Link from '@material-ui/core/Link'
 import { Link as RouterLink } from 'react-router-dom'
+import { WeatherDispatchContext, WeatherStateContext } from './../WeatherContext'
 
-const CityPage = ({actions, data}) => {
+const CityPage = () => {
+    const actions = useContext(WeatherDispatchContext)
+    const data = useContext(WeatherStateContext)
     const { allWeather, allChartData, allForecastItemList } = data
     // const { onSetAllWeather, onSetChartData, onSetForecastItemList } = actions
     const { city, countryCode } = useCityPage(allChartData, allForecastItemList, actions)
